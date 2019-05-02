a9c484b79287d7d2a67c02fd891c65949c2c1697
Haries Ramdhani <hydrolizedmaltose@gmail.com>
2019-05-03 00:02:35 +0700

    Changed the loading priority

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-import {indonesiaChoropleth} from "./indonesiaChoropleth.js";
 import {worldChoropleth} from "./worldChoropleth.js";
+import {indonesiaChoropleth} from "./indonesiaChoropleth.js";
 
 const dataPath = "src/assets/"
 const jsonDataPath = `${dataPath}/json`
 
-indonesiaChoropleth("indonesia-choropleth", `${jsonDataPath}/indonesia.json`);
 worldChoropleth("world-choropleth", `${jsonDataPath}/world.json`);
+indonesiaChoropleth("indonesia-choropleth", `${jsonDataPath}/indonesia.json`);
 
 console.log("Please let me know if the site isn't working properly thank you! - H")
